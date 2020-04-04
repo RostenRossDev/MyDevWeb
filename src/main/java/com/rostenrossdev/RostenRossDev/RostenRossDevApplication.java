8f20ea8d2079a2b5e2805b14452bd72fd75fdac9
RostenRossDev <61593273+RostenRossDev@users.noreply.github.com>
2020-04-04 11:06:36 -0300

    Project create by spring initializr
    For this project was used gitpod.io an Online IDE,  https://remotemysql.com/ like a data base,  thymeleaf, mysql and mvc model.

---
+++ src/main/java/com/rostenrossdev/RostenRossDev/RostenRossDevApplication.java
@@ -0,0 +1,13 @@
+package com.rostenrossdev.RostenRossDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RostenRossDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RostenRossDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rostenrossdev/RostenRossDev/RostenRossDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rostenrossdev.RostenRossDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RostenRossDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
