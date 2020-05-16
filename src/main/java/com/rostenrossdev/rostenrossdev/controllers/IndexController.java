4cbd70ca60ee3096b8b0d710fbb6f0338ca8f629
Nestor Matias Costantini <costantini.nestor.m@gmail.com>
2020-05-16 21:59:31 +0000

    import.sql was created.

--- src/main/java/com/rostenrossdev/rostenrossdev/controllers/IndexController.java
+++ src/main/java/com/rostenrossdev/rostenrossdev/controllers/IndexController.java
@@ -11,19 +11,23 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
 public class IndexController{
 
     @Autowired
-    private IPostServiceImpl poserService;
+    private IPostServiceImpl postService;
 
     @GetMapping("/home")
     public String index(@RequestParam(name = "page", defaultValue = "0") int page, Model model){
         Pageable pageRequest = PageRequest.of(page, 4);
-        Page<Post> posts = poserService.findAll(pageRequest);
+        Page<Post> posts = postService.findAll(pageRequest);
         PageRender<Post> pageRender = new PageRender<>("/home", posts);
         if (posts.isEmpty()) {
             model.addAttribute("posts","Por el momento no hay ningun proyecto para mostrar!!");
@@ -38,6 +42,22 @@ public class IndexController{
     @GetMapping(value={"/","","/index"})
     public String redirigirHome(){
 
+        return "redirect:/home";
+    }
+
+    @GetMapping("/nuevoPost")
+    public String nuevoPost(Model model,  RedirectAttributes flash){
+
+        
+        model.addAttribute("titulo", "Crear Factura");        
+
+        return "form_post";
+    }
+    @PostMapping("/guardarPost")
+    public String guardarPost(@RequestAttribute (value="psot") Post post, Model model, RedirectAttributes flash){
+        
+        postService.save(post);
+        flash.addAttribute("success","Post agregado con exito!!");
         return "redirect:/home";
     }
 }
