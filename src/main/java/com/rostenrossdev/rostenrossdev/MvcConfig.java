c4e4f999ccc3eb78193e769a784b6765635d7999
Nestor Matias Costantini <costantini.nestor.m@gmail.com>
2020-05-19 23:18:39 +0000

    Traduction of text in spanish and english was added.

--- src/main/java/com/rostenrossdev/rostenrossdev/MvcConfig.java
+++ src/main/java/com/rostenrossdev/rostenrossdev/MvcConfig.java
@@ -1,5 +1,6 @@
 package com.rostenrossdev.rostenrossdev;
 
+
 import java.util.Locale;
 
 import org.springframework.context.annotation.Bean;
@@ -11,7 +12,6 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
